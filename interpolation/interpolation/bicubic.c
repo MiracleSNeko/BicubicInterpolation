b3014449973c63d4a58386b8a7b95c8b553e9aa2
Yuchien Lin <39214153+MiracleSNeko@users.noreply.github.com>
2019-04-17 01:12:19 +0900

    change error handler

--- interpolation/interpolation/bicubic.c
+++ interpolation/interpolation/bicubic.c
@@ -25,17 +25,19 @@ typedef float ElemType;
  * @ brief:
 		双三次插值函数，返回在给定点的函数值估计 
 		输入： 待求值的点，插值区域网格点和函数值
-		输出： 待求值点的函数值估计
-		若待求值点已经在列表中，返回NaN
+		输出： 待求值点的函数值估计或精确值（查表）
  * @ change history:
 		<date> | <discription>
+		190416 | 更改错误处理，给定值在列表中时直接查表返回值
 
  */
 ElemType Bicubic(ElemType point_x, ElemType point_y, ElemType *mesh_x, ElemType *mesh_y, 
 				 ElemType *mesh_value) {
 	ElemType* position_in_mesh;
-	position_in_mesh = FindPointPosition(point_x, point_y, mesh_x, mesh_y);
-	if (NULL == position_in_mesh) return 0. / 0.;
+	int ierr;
+	size_t len_x = sizeof(mesh_x) / sizeof(ElemType);
+	position_in_mesh = FindPointPosition(point_x, point_y, mesh_x, mesh_y, ierr);
+	if (-1 == ierr) return *(mesh_value + (int)position_in_mesh[0] + len_x*(int)position_in_mesh[1]);
 	// 计算部分
 	free(position_in_mesh);
 	position_in_mesh = NULL;
@@ -46,23 +48,30 @@ ElemType Bicubic(ElemType point_x, ElemType point_y, ElemType *mesh_x, ElemType 
 		确定给出节点在已知网格中的带小数坐标
 		输入：待求值的点，插值区域网格点
 		输出：带小数坐标
-		如果给定点已经在列表中，返回空指针
+		如果给定点已经在列表中，返回整数坐标，flag赋值-1
  * @ change history:
 		<date> | <discription>
+		190416 | 更改错误处理，增加flag，给定值在列表
+				 中时flag赋值-1，返回整数坐标
 
  */
 ElemType* FindPointPosition(ElemType point_x, ElemType point_y, ElemType *mesh_x, 
-							ElemType *mesh_y) {
+							ElemType *mesh_y,  int flag) {
 	ElemType* position;
+	int ierr_x, ierr_y;
 	int position_int_x, position_int_y;
-	position_int_x = FindValuePositionInList(point_x, mesh_x);
-	position_int_y = FindValuePositionInList(point_y, mesh_y);
-	if (-1 == position_int_x || -1 == position_int_y) {
-		return NULL;
-	}
+	flag = 0;
+	position_int_x = FindValuePositionInList(point_x, mesh_x, ierr_x);
+	position_int_y = FindValuePositionInList(point_y, mesh_y, ierr_y);
 	position = (ElemType *)malloc(sizeof(ElemType) * 2);
-	position[0] = (ElemType)position_int_x + point_x / (mesh_x[position_int_x] + mesh_x[position_int_x + 1]);
-	position[1] = (ElemType)position_int_y + point_y / (mesh_y[position_int_y] + mesh_y[position_int_y + 1]);
+	position[0] = (ElemType)position_int_x;
+	position[1] = (ElemType)position_int_y;
+	if (-1 == ierr_x && -1 == ierr_y) {
+		flag = -1;
+		return position;
+	}
+	position[0] +=  point_x / (mesh_x[position_int_x] + mesh_x[position_int_x + 1]);
+	position[1] +=  point_y / (mesh_y[position_int_y] + mesh_y[position_int_y + 1]);
 	return position;
 }
 
@@ -71,16 +80,19 @@ ElemType* FindPointPosition(ElemType point_x, ElemType point_y, ElemType *mesh_x
 		在有序列表中查找值不大于给定数的最大元素的位置
 		输入： 给定数，给定列表
 		输出： 不大于给定数的最大元素索引的位置
-		如果给定值已经在列表中，返回-1
+		如果给定值已经在列表中，flag返回-1
  * @ change history:
 		<date> | <discription>
 		190415 | 更改返回值类型：ElemType -> int
+		190416 | 更改错误处理，增加flag，给定值在列表
+				 中时直接返回-1改为flag返回-1
 
  */
-int FindValuePositionInList(ElemType value, ElemType* list) {
+int FindValuePositionInList(ElemType value, ElemType* list, int flag) {
 	int cnt = 0;
-	int len = sizeof(list) / sizeof(ElemType);
+	flag = 0;
+	size_t len = sizeof(list) / sizeof(ElemType);
 	while (value > list[cnt]) ++cnt;
-	if (value == list[cnt]) cnt = -1;
+	if (value == list[cnt]) flag = -1;
 	return cnt;
 }
